5a6b32833e415dd5e2915a3b8458d6d3a7003834
Rebecca Samuel <se.rebecca.samuel@gmail.com>
2021-02-18 23:39:34 +0300

    started working on Lab 8

---
+++ asset/js/01(TemplateLiteral).js
@@ -0,0 +1,46 @@
+//DOM Load 
+document.addEventListener('DOMContentLoaded', () => {
+
+    templateStringConcatenation();
+
+    displayTemplateExpression();
+
+
+})
+
+
+function templateStringConcatenation() {
+
+    // A Student Object 
+    const student = { name: "Abebe", age: 21 }
+
+    //String to Display 
+    const fullIntro = "My Name is " + student.name + " and i am " + student.age + " Years Old"
+
+    //1. Create a variable called fullIntroTemplate and Store the same string displayed above with template literal
+    const fullIntroTemplate = `My Name is ${student.name} and I am ${student.age} Years Old`;
+
+    //3. Display it [change it to fullIntroTemplate]
+    templateConcatenationResult.innerHTML = fullIntroTemplate;
+
+}
+
+function displayTemplateExpression() {
+
+
+    //String to Display 
+    const result = "The Sum of the Numbers is  : " + sum(1, 2)
+
+    //1. Create a variable called result1 and Store the same string displayed above with template literal
+    const result1 = `The Sum of the numbers is : ${sum(1,2)}`
+
+    //3. Display it [change it to result1]
+    templateExpression.innerHTML = result1;
+
+}
+
+function sum(x, y) {
+
+    return x + y;
+
+}
